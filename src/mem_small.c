8b8bbe4e818b7a0d91c8f992aa245b29059796dc
angiebachetarzi <bachetarzi.angie@hotmail.com>
2020-01-26 16:57:35 +0100

    i think mem_small works

--- src/mem_small.c
+++ src/mem_small.c
@@ -11,27 +11,26 @@
 void *
 emalloc_small(unsigned long size)
 {
-    // if chunkpool is empty, allocate new chunkpool
-    // create new list for the chunkpool
+    //if the chunkpool is empty, allocate a new one
+    //create a new list for the chunkpool
     if (arena.chunkpool == NULL) {
-        unsigned long new_chunkpool_size = mem_realloc_small();
-        unsigned long chunk_number = new_chunkpool_size/CHUNKSIZE;
+        unsigned long new_chunkpool_size =  mem_realloc_small();
+        int chunk_number = (new_chunkpool_size / CHUNKSIZE);
 
-        //head of the list
-        void **ptr = (void **) arena.chunkpool;
+        void* current = arena.chunkpool;
 
-        for (int i = 0; i < chunk_number; i++) {
-            //actual ptr points to next one
-            *ptr = (void *)(ptr + CHUNKSIZE);
-            //move on to the next chunk
-            ptr = ptr + CHUNKSIZE;
+        for (int i = 0; i < chunk_number; i++){
+            //actual ptr points to the next one
+            *(void **)current = current + CHUNKSIZE;
+            //move to the next chunk
+            current = current + CHUNKSIZE;
         }
     }
-
-    //get the first chunk of the chunkpool
+    
+    //get the first chunk of the pool
     void *first = arena.chunkpool;
     //update the chunkpool to get the first ptr
-    arena.chunkpool = *(void **) arena.chunkpool;
+    arena.chunkpool = *(void**)first;
 
     //call function to mark the ptr
     return mark_memarea_and_get_user_ptr(first, CHUNKSIZE, SMALL_KIND);
@@ -39,8 +38,9 @@ emalloc_small(unsigned long size)
 }
 
 void efree_small(Alloc a) {
-    //get a to the head of the list
-    *(void **) a.ptr = arena.chunkpool;
+    //get the a to the head of the list
+    *(void **)a.ptr = arena.chunkpool;
+    
     //update the chunkpool
     arena.chunkpool = a.ptr;
 }
